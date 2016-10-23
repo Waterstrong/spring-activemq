066b241e3961cc638bac3f334921d3392898a873
Waterstrong <sqlin@thoughtworks.com>
2016-10-23 23:20:20 +0800

    init activemq project

---
+++ src/main/java/ws/demo/activemq/Application.java
@@ -0,0 +1,8 @@
+package ws.demo.activemq;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(Application.class, args);
+    }
+}
